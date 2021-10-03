ccf630ab57395f735622380044419aaf15ab3f3f
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-03 16:56:44 -0400

    Added Tasks 1 & 2

---
+++ LAB-03_Template/src/Task2.c
@@ -0,0 +1,17 @@
+//------------------------------------
+// Lab 3 - Task 2
+//------------------------------------
+//
+
+#include "init.h"
+
+// Main Execution Loop
+int main(void) {
+	//Initialize the system
+	Sys_Init();
+
+	printf("Test!\r\n");
+
+	// Lost? Check out uart.c!
+
+}
