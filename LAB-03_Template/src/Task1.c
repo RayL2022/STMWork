98a3e58d16dc10c25d0ed5617c430ee30c1659c9
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-04 20:18:33 -0400

    up1

--- LAB-03_Template/src/Task1.c
+++ LAB-03_Template/src/Task1.c
@@ -5,7 +5,7 @@
 #include "init.h"
 
 UART_HandleTypeDef UART6; //Handle type structure for USART6
-int input = 0; //For storing user inputs
+uint8_t input; //For storing user inputs
 char end_message[] = "Program Has Halted\r\n";
 // Main Execution Loop
 int main(void) {
@@ -15,22 +15,20 @@ int main(void) {
 	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 	HAL_UART_MspInit(&UART6); //Initialize GPIOs for USART6
 	while (1){
-		//input = getchar();
-		//input = uart_getchar(&UART6, 1); //Get a char and echo the char
-		if (input != 0){
-			HAL_UART_Receive(&UART6, (uint8_t*) input, 1, 1000);
-		}
-		//HAL_UART_Transmit(&UART6, (uint8_t*) input, 1, 1000);
+		HAL_UART_Receive(&UART6, &input, 1, 1000);
+		HAL_UART_Transmit(&UART6, &input, 1, 1000);
 
+		/*
 		if (input == '\033'){ //When Escape key is pressed
 			HAL_UART_Transmit(&UART6, (uint8_t*) end_message, strlen((const char*)end_message), 1000);
-			//uart_print(&UART6, end_message); //Tell peripheral to print end_message
 			printf("%s",end_message); //Print on controller end
 			return 1;
 		}
-		else{
+		*/
+		//else{
 			printf("%c\r\n", input); //Print on controller end
-		}
+		//}
+
 
 	}
 
