107776f1a194c65b45673072bd8c162d3299ccf3
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-04 20:23:58 -0400

    up1

--- LAB-03_Template/src/Task1.c
+++ LAB-03_Template/src/Task1.c
@@ -5,7 +5,7 @@
 #include "init.h"
 
 UART_HandleTypeDef UART6; //Handle type structure for USART6
-uint8_t input; //For storing user inputs
+char input; //For storing user inputs
 char end_message[] = "Program Has Halted\r\n";
 // Main Execution Loop
 int main(void) {
@@ -15,21 +15,18 @@ int main(void) {
 	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 	HAL_UART_MspInit(&UART6); //Initialize GPIOs for USART6
 	while (1){
-		HAL_UART_Receive(&UART6, &input, 1, 1000);
-		HAL_UART_Transmit(&UART6, &input, 1, 1000);
-
+		HAL_UART_Receive(&USB_UART, (uint8_t*) &input, 1, 10);
+		HAL_UART_Transmit(&UART6, (uint8_t*) &input, 1, 10);
 		/*
 		if (input == '\033'){ //When Escape key is pressed
 			HAL_UART_Transmit(&UART6, (uint8_t*) end_message, strlen((const char*)end_message), 1000);
 			printf("%s",end_message); //Print on controller end
 			return 1;
 		}
-		*/
-		//else{
+		else{
 			printf("%c\r\n", input); //Print on controller end
-		//}
-
-
+		}
+		 */
 	}
 
 }
