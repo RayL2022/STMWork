ed942de57fb07c1d503974e5bd8c4bf5af2b71aa
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-07 15:45:25 -0400

    Task 1 Changes

--- LAB-03_Template/src/Task1.c
+++ LAB-03_Template/src/Task1.c
@@ -4,19 +4,25 @@
 //
 #include "init.h"
 
-UART_HandleTypeDef UART6; //Handle type structure for USART6
+UART_HandleTypeDef U6; //Handle type structure for USART6
 char input; //For storing user inputs
-char end_message[] = "Program Has Halted\r\n";
+//char end_message[] = "Program Has Halted\r\n";
+
 // Main Execution Loop
 int main(void) {
 	//Initialize the system
 	Sys_Init();
+	HAL_Init();
 
-	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
-	HAL_UART_MspInit(&UART6); //Initialize GPIOs for USART6
+	//initUart(&USB_UART, 115200, USART1);
+	//HAL_UART_MspInit(&USB_UART);
+
+	initUart(&U6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
+	HAL_UART_MspInit(&U6); //Initialize GPIOs for USART6
 	while (1){
 		HAL_UART_Receive(&USB_UART, (uint8_t*) &input, 1, 10);
-		HAL_UART_Transmit(&UART6, (uint8_t*) &input, 1, 10);
+		HAL_UART_Transmit(&U6, (uint8_t*) &input, 1, 10);
+
 		/*
 		if (input == '\033'){ //When Escape key is pressed
 			HAL_UART_Transmit(&UART6, (uint8_t*) end_message, strlen((const char*)end_message), 1000);
