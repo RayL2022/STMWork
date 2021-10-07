33d230fedb0c2a076fd5f2bef886552f67602a95
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-07 18:36:43 -0400

    task2

--- LAB-03_Template/src/Task2.c
+++ LAB-03_Template/src/Task2.c
@@ -4,49 +4,65 @@
 //
 #include "init.h"
 
-UART_HandleTypeDef UART6; //Handle type structure for USART6
-UART_HandleTypeDef UART1; //Handle type structure for USART1
-
+UART_HandleTypeDef U6; //Handle type structure for USART6
+char input = 0;
+int end();
+int halt;
 // Main Execution Loop
 int main(void) {
 	//Initialize the system
 	Sys_Init();
-	Hal_Init();
-	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
-	HAL_UART_MspInit(&UART6); //Initialize GPIOs for USART6
 
-	initUart(&USB_UART, 115200, USART1); //Initialize UART with 38400 Baud and Instance USART1
-	HAL_UART_MspInit(&UART6); //Initialize GPIOs for USART1
+	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 
 	HAL_NVIC_EnableIRQ(USART1_IRQn);
-
-	printf("Test!\r\n");
+	HAL_NVIC_EnableIRQ(USART6_IRQn);
 
 	while(1);
-	// Lost? Check out uart.c!
 
+}
+int end(){
+	if (input == '\033'){ //When Escape key is pressed
+		HAL_UART_Transmit(&U6, (uint8_t*) end_message, strlen((const char*)end_message), 10);
+		HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
+		return 1;
+	}
+	return 0;
 }
 
 
 void USART1_IRQHandler() {
 	HAL_UART_IRQHandler(&USB_UART);
-	printf("Test1\n\r");
 }
 
 void USART6_IRQHandler() {
-	printf("Test2\n\r");
-	HAL_UART_IRQHandler(&UART6);
+	HAL_UART_IRQHandler(&U6);
 }
 
 HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if (huart->Instance == USART1){
-		printf("TestUSART1\n\r");
-		HAL_UART_Transmit_IT();
+		HAL_UART_Receive_IT(&USB_UART, (uint8_t*) &input, 1, 10); //My User
+		if (input){
+			halt = end();
+			if (halt == 1) {
+				break;
+			}
+			HAL_UART_Transmit_IT(&U6, (uint8_t*) &input, 1, 10);
+			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1, 2);
+			input = 0;
+		}
 	}
 
 	if (huart->Instance == USART6){
-		printf("TestUSART6\n\r");
-		HAL_UART_Receive_IT();
+		HAL_UART_Receive(&U6, (uint8_t*) &input, 1, 10); //Other User
+		if (input){
+			halt = end();
+			if (halt == 1){
+				break;
+			}
+			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 10);
+			input = 0;
+		}
 	}
 }
 
