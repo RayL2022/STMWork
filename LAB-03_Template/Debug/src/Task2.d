5e78ff28e78cec21f793c281c09f2d8d83cd1d8a
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-07 18:42:12 -0400

    task2

---
+++ LAB-03_Template/Debug/src/Task2.d
@@ -0,0 +1,91 @@
+src/Task2.o: ../src/Task2.c \
+ E:/Local_REPO/STMWork/LAB-03_Template/inc/init.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/core_cm7.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_version.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_compiler.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_gcc.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/mpu_armv7.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ E:/Local_REPO/STMWork/LAB-03_Template/inc/stm32f7xx_hal_conf.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi_ex.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
+ E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h \
+ E:/Local_REPO/STMWork/LAB-03_Template/inc/uart.h
+
+E:/Local_REPO/STMWork/LAB-03_Template/inc/init.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/core_cm7.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_version.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_compiler.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/cmsis_gcc.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Include/mpu_armv7.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+E:/Local_REPO/STMWork/LAB-03_Template/inc/stm32f7xx_hal_conf.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+E:/Local_REPO/STMWork/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi_ex.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h:
+
+E:/Local_REPO/STMWork/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
+
+E:/Local_REPO/STMWork/LAB-03_Template/inc/uart.h:
--- LAB-03_Template/src/Task2.c
+++ LAB-03_Template/src/Task2.c
@@ -6,6 +6,7 @@
 
 UART_HandleTypeDef U6; //Handle type structure for USART6
 char input = 0;
+char end_message[18] = {'P','r','o','g','r','a','m',' ','H','a','s',' ','H','a','l','t','e','d'};
 int end();
 int halt;
 // Main Execution Loop
@@ -13,21 +14,13 @@ int main(void) {
 	//Initialize the system
 	Sys_Init();
 
-	initUart(&UART6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
+	initUart(&U6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 
 	HAL_NVIC_EnableIRQ(USART1_IRQn);
 	HAL_NVIC_EnableIRQ(USART6_IRQn);
 
 	while(1);
 
-}
-int end(){
-	if (input == '\033'){ //When Escape key is pressed
-		HAL_UART_Transmit(&U6, (uint8_t*) end_message, strlen((const char*)end_message), 10);
-		HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
-		return 1;
-	}
-	return 0;
 }
 
 
@@ -39,28 +32,20 @@ void USART6_IRQHandler() {
 	HAL_UART_IRQHandler(&U6);
 }
 
-HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if (huart->Instance == USART1){
-		HAL_UART_Receive_IT(&USB_UART, (uint8_t*) &input, 1, 10); //My User
+		HAL_UART_Receive_IT(&USB_UART, (uint8_t*) &input, 1); //My User
 		if (input){
-			halt = end();
-			if (halt == 1) {
-				break;
-			}
-			HAL_UART_Transmit_IT(&U6, (uint8_t*) &input, 1, 10);
-			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1, 2);
+			HAL_UART_Transmit_IT(&U6, (uint8_t*) &input, 1);
+			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1);
 			input = 0;
 		}
 	}
 
 	if (huart->Instance == USART6){
-		HAL_UART_Receive(&U6, (uint8_t*) &input, 1, 10); //Other User
+		HAL_UART_Receive_IT(&U6, (uint8_t*) &input, 1); //Other User
 		if (input){
-			halt = end();
-			if (halt == 1){
-				break;
-			}
-			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 10);
+			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1);
 			input = 0;
 		}
 	}
