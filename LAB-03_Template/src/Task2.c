08fb419483a38af2559e11278a3cad11cfb904d3
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-07 20:19:15 -0400

    tsk2

--- LAB-03_Template/src/Task2.c
+++ LAB-03_Template/src/Task2.c
@@ -5,19 +5,20 @@
 #include "init.h"
 
 UART_HandleTypeDef U6; //Handle type structure for USART6
-char input = 0;
-char end_message[18] = {'P','r','o','g','r','a','m',' ','H','a','s',' ','H','a','l','t','e','d'};
-int end();
-int halt;
+char input[4];
 // Main Execution Loop
 int main(void) {
 	//Initialize the system
 	Sys_Init();
+	HAL_Init();
 
 	initUart(&U6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 
 	HAL_NVIC_EnableIRQ(USART1_IRQn);
 	HAL_NVIC_EnableIRQ(USART6_IRQn);
+
+	HAL_UART_Receive_IT (&USB_UART, (uint8_t*) input, 1);
+	HAL_UART_Receive_IT (&U6, (uint8_t*) input, 1);
 
 	while(1);
 
@@ -25,29 +26,30 @@ int main(void) {
 
 
 void USART1_IRQHandler() {
+	//printf("IRQ1\r\n");
 	HAL_UART_IRQHandler(&USB_UART);
 }
 
 void USART6_IRQHandler() {
+	//printf("IRQ6\r\n");
 	HAL_UART_IRQHandler(&U6);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if (huart->Instance == USART1){
 		HAL_UART_Receive_IT(&USB_UART, (uint8_t*) &input, 1); //My User
-		if (input){
+		if (input[0]){
 			HAL_UART_Transmit_IT(&U6, (uint8_t*) &input, 1);
 			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1);
-			input = 0;
+			input[0] = 0;
 		}
 	}
 
 	if (huart->Instance == USART6){
 		HAL_UART_Receive_IT(&U6, (uint8_t*) &input, 1); //Other User
-		if (input){
+		if (input[0]){
 			HAL_UART_Transmit_IT(&USB_UART, (uint8_t*) &input, 1);
-			input = 0;
+			input[0] = 0;
 		}
 	}
 }
-
