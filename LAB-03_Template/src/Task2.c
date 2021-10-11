7c6f4aae2907390e0e529c0ff70abbd26b7586e8
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-10-11 13:29:26 -0400

    Update Task2.c

--- LAB-03_Template/src/Task2.c
+++ LAB-03_Template/src/Task2.c
@@ -5,67 +5,57 @@
 #include "init.h"
 
 UART_HandleTypeDef U6; //Handle type structure for USART6
-char input[4];
-int end = 0;
+char input;
+char end_message[18] = {'P','r','o','g','r','a','m',' ','H','a','s',' ','H','a','l','t','e','d'};
+char end = 0;
 // Main Execution Loop
 int main(void) {
 	//Initialize the system
 	Sys_Init();
 	HAL_Init();
-
 	initUart(&U6, 38400, USART6); //Initialize UART with 38400 Baud and Instance USART6
 
 	HAL_NVIC_EnableIRQ(USART1_IRQn);
 	HAL_NVIC_EnableIRQ(USART6_IRQn);
 
-	HAL_UART_Receive_IT (&USB_UART, (uint8_t*) input, 1);
-	HAL_UART_Receive_IT (&U6, (uint8_t*) input, 1);
+	HAL_UART_Receive_IT (&USB_UART, (uint8_t*) &input, 1);
+	HAL_UART_Receive_IT (&U6, (uint8_t*) &input, 1);
 
-	while(1){
-		if (end == 1){
-			printf("Carried out");
-			break;
-		}
-	}
-
-	printf ("Program Has Halted"); fflush(stdout);
-
+	while(end == 0);
 }
 
 
 void USART1_IRQHandler() {
-	//printf("IRQ1\r\n");
 	HAL_UART_IRQHandler(&USB_UART);
 }
 
 void USART6_IRQHandler() {
-	//printf("IRQ6\r\n");
 	HAL_UART_IRQHandler(&U6);
-	//printf("out\n\r");
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-	printf("callback");
 	if (huart->Instance == USART1){
 		HAL_UART_Receive_IT(&USB_UART, (uint8_t*) &input, 1); //My User
-		if (input[0] == '\033'){
-			end = 1;
-		}
-		else if (input[0]){
-			HAL_UART_Transmit(&U6, (uint8_t*) input, 1, 10);
+		if (input){
+			if (input == '\033'){
+				end = 1;
+				HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
+			}
+			HAL_UART_Transmit(&U6, (uint8_t*) &input, 1, 10);
 			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 10);
-			input[0] = 0;
+			input = 0;
 		}
 	}
 
 	if (huart->Instance == USART6){
 		HAL_UART_Receive_IT(&U6, (uint8_t*) &input, 1); //Other User
-		if (input[0] == '\033'){
-			end = 1;
-		}
-		else if (input[0]){
+		if (input){
+			if (input == '\033'){
+				end = 1;
+				HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
+			}
 			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 10);
-			input[0] = 0;
-		}
+			input = 0;
+			}
 	}
 }
