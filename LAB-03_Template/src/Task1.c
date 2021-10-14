79442c216fc9f143ae04fe9f94bae21418e79c92
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-10-14 17:51:27 -0400

    Update Task1.c

--- LAB-03_Template/src/Task1.c
+++ LAB-03_Template/src/Task1.c
@@ -8,8 +8,7 @@ UART_HandleTypeDef U6; //Handle type structure for USART6'
 //For storing user inputs
 char end_message[] = "\r\nProgram Has Halted";
 char input;
-int end();
-int halt;
+char end = 0;
 // Main Execution Loop
 int main(void) {
 	//Initialize the system
@@ -21,12 +20,12 @@ int main(void) {
 
 	//HAL_UART_MspInit(&U6); //Initialize GPIOs for USART6
 	input = 0;
-	while (1){
+	while (end == 0){
 		HAL_UART_Receive(&USB_UART, (uint8_t*) &input, 1, 10); //My User
 		if (input){
-			halt = end();
-			if (halt == 1) {
-				break;
+			if (input == '\033'){
+				end = 1;
+				HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
 			}
 			HAL_UART_Transmit(&U6, (uint8_t*) &input, 1, 10);
 			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 2);
@@ -35,9 +34,9 @@ int main(void) {
 
 		HAL_UART_Receive(&U6, (uint8_t*) &input, 1, 10); //Other User
 		if (input){
-			halt = end();
-			if (halt == 1){
-				break;
+			if (input == '\033'){
+				end = 1;
+				HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
 			}
 			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 10);
 			input = 0;
@@ -45,11 +44,3 @@ int main(void) {
 	}
 }
 
-int end(){
-	if (input == '\033'){ //When Escape key is pressed
-		HAL_UART_Transmit(&U6, (uint8_t*) end_message, strlen((const char*)end_message), 10);
-		HAL_UART_Transmit(&USB_UART, (uint8_t*) end_message, strlen((const char*)end_message), 10);
-		return 1;
-	}
-	return 0;
-}
