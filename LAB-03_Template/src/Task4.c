f329ca101a8ab696b76c736a8f963601c684f9d0
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-10-16 20:42:39 -0400

    Update Task4.c

--- LAB-03_Template/src/Task4.c
+++ LAB-03_Template/src/Task4.c
@@ -6,3 +6,88 @@
  */
 
 
+#include "init.h"
+
+// If needed:
+//#include <stdio.h>
+//#include <stdlib.h>
+
+SPI_HandleTypeDef S2;
+char input;
+char spi_data;
+
+/*
+ * For convenience, configure the SPI handler here
+ */
+// See 769 Description of HAL drivers.pdf, Ch. 58.1 or stm32f7xx_hal_spi.c
+void configureSPI()
+{
+	S2.Instance = SPI2; // Please use SPI2!
+	S2.Init.Mode = SPI_MODE_MASTER; // Set master mode
+	S2.Init.TIMode = SPI_TIMODE_DISABLE; // Use Motorola mode, not TI mode
+	S2.Init.Direction = SPI_DIRECTION_2LINES; //2 separate lines, transmit and receive at same time
+	S2.Init.DataSize = SPI_DATASIZE_8BIT; //transfer/receive 8 bits at a time
+	S2.Init.NSS = SPI_NSS_SOFT; //Using software to select peripheral
+	S2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128; //~0.84 MHz ~ 1 Mhz given noise
+	S2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE; //Disable CRC
+	HAL_SPI_Init(&S2);
+	//
+	// Note: HAL_StatusTypeDef HAL_SPI_Init(SPI_HandleTypeDef *hspi)
+	//
+	// HAL_SPI_Init() will call HAL_SPI_MspInit() after reading the properties of
+	// the passed SPI_HandleTypeDef. After finishing the MspInit call, it will set
+	// the SPI property bits. This is how all HAL_[peripheral]_Init() functions work.
+}
+
+/*
+ * This is called upon SPI initialization. It handles the configuration
+ * of the GPIO pins for SPI.
+ */
+ // Do NOT change the name of an MspInit function; it needs to override a
+ // __weak function of the same name. It does not need a prototype in the header.
+void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
+{
+	// SPI GPIO initialization structure here
+	GPIO_InitTypeDef  GPIO_InitStruct;
+
+	if (hspi->Instance == SPI2)
+	{
+		// Enable GPIO Clocks
+		__GPIOA_CLK_ENABLE();
+		__GPIOJ_CLK_ENABLE();
+		__GPIOB_CLK_ENABLE();
+		__GPIOC_CLK_ENABLE();
+
+		GPIO_InitStruct.Pin       = GPIO_PIN_12;  //D13 SCLK
+		GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull      = GPIO_NOPULL;
+		GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+		GPIO_InitStruct.Alternate = GPIO_AF5_SPI2; //Has to be 5
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		// Initialize RX Pin
+		GPIO_InitStruct.Pin = GPIO_PIN_14;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D12 SDO
+
+		GPIO_InitStruct.Pin = GPIO_PIN_15;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D11 SDI
+
+		GPIO_InitStruct.Pin = GPIO_PIN_3;
+		HAL_GPIO_Init(GPIOJ, &GPIO_InitStruct); //J3 DEBUG
+
+		GPIO_InitStruct.Pin = GPIO_PIN_2;
+		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //A2 CS
+
+		// Enable SPI Clocking
+		__SPI2_CLK_ENABLE();
+
+	}
+}
+
+int main(void)
+{
+
+
+
+
+}
