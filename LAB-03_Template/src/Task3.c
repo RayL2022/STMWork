e20e5e47c7ff3f786df803a561975c1989ab1933
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-10-17 14:52:09 -0400

    Update Task3

--- LAB-03_Template/src/Task3.c
+++ LAB-03_Template/src/Task3.c
@@ -28,6 +28,11 @@ void configureSPI()
 	S2.Init.NSS = SPI_NSS_SOFT; //Using software to select peripheral
 	S2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128; //~0.84 MHz ~ 1 Mhz given noise
 	S2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE; //Disable CRC
+
+	S2.Init.CLKPhase = SPI_PHASE_1EDGE;
+	S2.Init.CLKPolarity = SPI_POLARITY_LOW;
+	S2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	S2.Init.CRCPolynomial = 10;
 	HAL_SPI_Init(&S2);
 	//
 	// Note: HAL_StatusTypeDef HAL_SPI_Init(SPI_HandleTypeDef *hspi)
@@ -58,23 +63,23 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 
 		GPIO_InitStruct.Pin       = GPIO_PIN_12;  //D13 SCLK
 		GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull      = GPIO_NOPULL;
+		GPIO_InitStruct.Pull      = GPIO_PULLUP;
 		GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
 		GPIO_InitStruct.Alternate = GPIO_AF5_SPI2; //Has to be 5
 		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 		// Initialize RX Pin
 		GPIO_InitStruct.Pin = GPIO_PIN_14;
-		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D12 SDO
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D12 MISO
 
 		GPIO_InitStruct.Pin = GPIO_PIN_15;
-		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D11 SDI
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //D11 MOSI
 
 		GPIO_InitStruct.Pin = GPIO_PIN_3;
-		HAL_GPIO_Init(GPIOJ, &GPIO_InitStruct); //J3 DEBUG
+		HAL_GPIO_Init(GPIOJ, &GPIO_InitStruct); //D7 DEBUG
 
-		GPIO_InitStruct.Pin = GPIO_PIN_2;
-		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //A2 CS
+		GPIO_InitStruct.Pin = GPIO_PIN_11;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //A2 CS
 
 		// Enable UART Clocking
 		__SPI2_CLK_ENABLE();
@@ -88,73 +93,48 @@ int main(void)
 	HAL_Init();
 
 	configureSPI();
-
 	HAL_NVIC_EnableIRQ(SPI2_IRQn);
-	HAL_NVIC_EnableIRQ(USART1_IRQn);
 
 	input = 0;
-	while (1){
-		HAL_UART_Receive(&USB_UART, (uint8_t*) &input, 1, 10); //UART Input
 
+	while(1){
+		HAL_UART_Receive (&USB_UART, (uint8_t*) &input, 1, 10);
 		if (input){
-			//printf("Printing 1: %x\r\n", input);
-/*			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
-			HAL_Delay(1);*/
+			printf("\033[0;0H"); fflush(stdout);
+			HAL_UART_Transmit (&USB_UART, (uint8_t*) &input, 1, 10);
+
+			//Receive Input in Peripheral Mode, Transmit(store) copied data into spi_data_peri
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+			HAL_Delay(1);
 			HAL_SPI_TransmitReceive(&S2, (uint8_t*) &input, (uint8_t*) &spi_data, 1, 10);
-/*			HAL_Delay(1);
-			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);*/
+			HAL_Delay(1);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
 
-/*			//printf("Printing 2: %x\r\n", input);
-			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
-			HAL_Delay(1);*/
-			//HAL_SPI_Transmit(&S2, (uint8_t*) &spi_data, 1, 10);
-/*			HAL_Delay(1);
-			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);*/
+			//Transmit from peripheral the stored data to Master
+			//HAL_SPI_TransmitReceive(&S2, (uint8_t*) &spi_data_peri, (uint8_t*) &spi_data_mast, 1, 10);
 
-			//printf("Printing 3: %x\r\n", input);
-			HAL_UART_Transmit(&USB_UART, (uint8_t*) &input, 1, 2);
 			input = 0;
 
-			//printf("\r\nPrinting 4: %x\r\n", input);
 		}
 
-/*
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+/*		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
 		HAL_Delay(1);
-		HAL_SPI_Receive(&S2, (uint8_t*) &input, 1, 10); //SPI Input
+		HAL_SPI_Receive(&S2, (uint8_t*) &spi_data, 1, 10);
+		HAL_SPI_Receive(&S2, (uint8_t*) &input, 1, 10);
 		HAL_Delay(1);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
-*/
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);*/
+
 		if (spi_data){
-			//printf("\r\nPrinting 5: %x\r\n", input);
-			HAL_UART_Transmit(&USB_UART, (uint8_t*) &spi_data, 1, 10);
+			printf("\033[12;0H"); fflush(stdout);
+			HAL_UART_Transmit (&USB_UART, (uint8_t*) &spi_data, 1, 10);
 			spi_data = 0;
 		}
 	}
+
+}
+
 // See 769 Description of HAL drivers.pdf, Ch. 58.2.3 or stm32f7xx_hal_spi.c
 //
 //	HAL_StatusTypeDef HAL_SPI_TransmitReceive(SPI_HandleTypeDef *hspi, uint8_t *pTxData, uint8_t *pRxData, uint16_t Size, uint32_t Timeout)
 //
-}
 
-/*
-
-void SPI2_IRQHandler() {
-	printf("Interrupt\r\n");
-	HAL_SPI_IRQHandler(&S2);
-}
-
-void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
-	printf("Callback\r\n");
-
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
-	HAL_Delay(1);
-	HAL_SPI_Receive (&S2, (uint8_t*)&rx_buffer, 1, 10);
-	HAL_UART_Transmit(&USB_UART, (uint8_t*) &rx_buffer, 1, 10);
-//	printf("%x",rx_buffer[0]);
-	HAL_SPI_Transmit (&S2, (uint8_t*)&rx_buffer, 1, 10);
-	HAL_Delay(1);
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
-}
-
-*/
