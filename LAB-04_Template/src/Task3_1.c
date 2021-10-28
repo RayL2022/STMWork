57ce6b7ff0ac989f5952fa9d9453a8057228567e
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-10-28 16:06:26 -0400

    Update Task3_1.c

--- LAB-04_Template/src/Task3_1.c
+++ LAB-04_Template/src/Task3_1.c
@@ -9,11 +9,11 @@
 //Result global variables
 int result1;
 volatile int32_t result2;
-volatile float result3;
-volatile float result4;
+volatile int32_t result3;
+volatile int32_t result4;
 
 //Operands variables
-volatile float num1, num2, num3, num4, x;
+volatile int32_t num1, num2, num3, num4, x;
 
 void do_task1(void);
 void do_task2(void);
@@ -27,8 +27,8 @@ int main(void)
 	Sys_Init();
 
 	//do_task1();
-	do_task2();
-	//do_task3();
+	//do_task2();
+	do_task3();
 	//do_task4();
 
 
@@ -43,19 +43,19 @@ void do_task1(void){
 	asm("ADD r4, r2, r3"); //Add 24 + 26, store in reg 4
 
 	asm("STR r4,%0" : "=m" (result1)); //Store result of addition in result1
-	printf("Result 1: %d\r\n", result1); //Should be 50
+	printf("Result 1: %d\r\n", result1); //Should be 60
 }
 
 void do_task2(void){
 	//Task 3 - Task Set 1 - #2
 	result2 = 0;
-	num1 = 0x00000026;
-	num2 = 0x00000026;
+	num1 = 11;
+	num2 = 11;
 
 	asm volatile("MUL %[result], %[oper1], %[oper2]"
 		: [result] "+r" (result2) //Read and Written to register, result2
 		: [oper1] "r" (num1), [oper2] "r" (num2)); //Only read from num1, num2
-	printf("Result 2: %ld\r\n", result2); //Result2 should be 0.75
+	printf("Result 2: %ld\r\n", result2); //Result2 should be 121
 }
 
 void do_task3(void){
@@ -64,23 +64,23 @@ void do_task3(void){
 	num1 = 2;
 	num2 = 3;
 	num3 = 5;
-	x = 1.5;
+	x = 12;
 
-	//Dividing past result by 3
-	asm volatile ("VDIV.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (result3)
-			: [oper1] "t" (num1), [oper2] "t" (num2));
+	//Multiplying 2 with x (x = 12)
+	asm volatile ("MUL %[result], %[oper1], %[oper2]"
+			: [result] "+r" (result3)
+			: [oper1] "r" (x), [oper2] "r" (num1));
 
-	//Multiplying 2 with x (x = 1.5)
-	asm volatile ("VMUL.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (result3)
-			: [oper1] "t" (result3), [oper2] "t" (x));
+	//Dividing past result by 3 (=8)
+	asm volatile ("SDIV %[result], %[oper1], %[oper2]"
+			: [result] "+r" (result3)
+			: [oper1] "r" (result3), [oper2] "r" (num2));
 
-	//Adding 5 to Result
-	asm volatile ("VADD.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (result3)
-			: [oper1] "t" (result3), [oper2] "t" (num3));
-	printf("Result 3: %f\r\n", (double)result3); //Printing, total should be 6
+	//Adding 5 to Result (=13)
+	asm volatile ("ADD %[result], %[oper1], %[oper2]"
+			: [result] "+r" (result3)
+			: [oper1] "r" (result3), [oper2] "r" (num3));
+	printf("Result 3: %ld\r\n", result3); //Printing, total should be 13
 }
 
 void do_task4(void){
@@ -90,18 +90,18 @@ void do_task4(void){
 	//x = 3; //Distinction from #3, should be 7 for result
 
 	//Doesn't change, still need to divide by 3
-	asm volatile ("VDIV.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (num1)
-			: [oper1] "t" (num1), [oper2] "t" (num2));
+	asm volatile ("DIV %[result], %[oper1], %[oper2]"
+			: [result] "+r" (num1)
+			: [oper1] "r" (num1), [oper2] "r" (num2));
 
 	//We can now multiply result with x and add to result sum
-	asm volatile ("VMLA.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (result4)
-			: [oper1] "t" (num1), [oper2] "t" (x));
+	asm volatile ("MLA %[result], %[oper1], %[oper2]"
+			: [result] "+r" (result4)
+			: [oper1] "r" (num1), [oper2] "r" (x));
 
 	//Multiply 5 by 1 and Add to result (pure addition to result with MAC)
-	asm volatile ("VMLA.F32 %[result], %[oper1], %[oper2]"
-			: [result] "+t" (result4)
-			: [oper1] "t" (num4), [oper2] "t" (num3));
-	printf("Result 4: %f\r\n", (double)result4); //Printing, total should be 6, as shown in #3
+	asm volatile ("MLA %[result], %[oper1], %[oper2]"
+			: [result] "+r" (result4)
+			: [oper1] "r" (num4), [oper2] "r" (num3));
+	printf("Result 4: %ld\r\n", result4); //Printing, total should be 6, as shown in #3
 }
