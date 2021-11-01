a0b79f0d07d2702c3a81beac8788c81c1ea92cc6
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-11-01 17:51:28 -0400

    comments

--- LAB-04_Template/src/Task1.c
+++ LAB-04_Template/src/Task1.c
@@ -6,7 +6,7 @@
 
 #include "init.h"
 ADC_HandleTypeDef hadc1;
-ADC_ChannelConfTypeDef sConfig;
+ADC_ChannelConfTypeDef sConfig;  //Configre handletypes as globals
 double avg = 0;
 double total;
 int count = 0;
@@ -14,7 +14,7 @@ double max = -100;
 double min = 10000;
 int i = 1;
 int slow = 0;
-double calc;
+double calc;	//variables used for additional calculations
 
 void configureADC();
 void PB_config();
@@ -27,32 +27,30 @@ int main(void)
 	configureADC();
 	PB_config();
 
-	// Code goes here
-	HAL_ADC_Start(&hadc1);
+	HAL_ADC_Start(&hadc1);	//Start ADC
 	while(1){
 		count=0;
-		//char msg[20];
 		uint16_t rawValue;
-		HAL_ADC_PollForConversion(&hadc1, 2000);
-		while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET){
-			if (count == 0){
-				rawValue = (float) HAL_ADC_GetValue(&hadc1);
-				calc = (rawValue*3.3)/4095;
+		HAL_ADC_PollForConversion(&hadc1, 2000);	//Continuously Poll for conversion
+		while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_SET){ //When the button is pushed
+			if (count == 0){ //Only once per button press
+				rawValue = (float) HAL_ADC_GetValue(&hadc1); //store value from ADC into variable
+				calc = (rawValue*3.3)/4095;	//convert value into voltage measurement
 				if (calc < 0){
 					calc = 0;
 				}
 				if (calc > 3.3){
-					calc = 3.3;
+					calc = 3.3;		//keep value within range of 0V to 3.3V
 				}
-				total = (calc + total);
-				avg = total/i;
+				total = (calc + total);	//running total of all measurements
+				avg = total/i;	//total value divided by number of values added
 				i = i + 1;
 				rawValue = (float)rawValue;
 				if (calc < min){
-					min = calc;
+					min = calc;	//check for new min
 				}
 				if (calc > max){
-					max = calc;
+					max = calc;	//check for new max
 				}
 
 				printf("\033[2J"); fflush(stdout);
@@ -60,7 +58,7 @@ int main(void)
 				printf("Decimal: %.5f\n\r", calc);
 				printf("Max: %.5f\n\r", max);
 				printf("Min: %.5f\n\r", min);
-				printf("Average: %.5f\n\r", avg);
+				printf("Average: %.5f\n\r", avg);	//Print all values required
 				count = 1;
 			}
 
