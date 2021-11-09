3695291bc768ca671c9c52ca5b435141c932af5a
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-11-09 12:26:26 -0500

    t1(working)

--- Lab05_DMA_Template/LAB-05-Template/src/Task1.c
+++ Lab05_DMA_Template/LAB-05-Template/src/Task1.c
@@ -3,9 +3,9 @@
 #include<stdlib.h>
 
 DMA_HandleTypeDef hdma1;
-uint8_t data[20] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-uint8_t dma_buffer[20];
-uint8_t c_buffer[20];
+uint8_t data[10] = {0xff, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t dma_buffer[10];
+uint8_t c_buffer[10];
 
 void configureDMA();
 
@@ -21,9 +21,9 @@ int main(void)
 
 	// C-Implementation without DMA
 	DWT->CYCCNT = 0; // Clear the cycle counter
-/*	for (int i = 0; i < 20; i++){
+	for (int i = 0; i < sizeof(data); i++){
 		c_buffer[i] = data[i];
-	}*/
+	}
 	uint32_t cycles = DWT->CYCCNT; // Store the cycle counter
 
 	DWT->CYCCNT = 0; // Clear the cycle counter
@@ -37,7 +37,7 @@ int main(void)
 
 
 	DWT->CYCCNT = 0; // Clear the cycle counter
-	HAL_DMA_Start(&hdma1, (uint32_t)&data, (uint32_t)&dma_buffer, 20);
+	HAL_DMA_Start(&hdma1, (uint32_t)&data, (uint32_t)&dma_buffer, 10);
 	HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
 	cycles = DWT->CYCCNT;
 
@@ -49,7 +49,6 @@ int main(void)
 	printf("And printing the above took %lu CPU cycles\r\n",cycles);
 	fflush(stdout);
 
-	while(1);
 }
 
 
@@ -58,19 +57,21 @@ void configureDMA(){
 	__HAL_RCC_DMA2_CLK_ENABLE();
 
 	hdma1.Instance = DMA2_Stream0;
-	hdma1.Init.Channel = DMA_CHANNEL_0;
+	hdma1.Init.Channel = DMA_CHANNEL_1;
 	hdma1.Init.Direction = DMA_MEMORY_TO_MEMORY;
-	hdma1.Init.PeriphInc = DMA_PINC_DISABLE;
+	hdma1.Init.PeriphInc = DMA_PINC_ENABLE;
 	hdma1.Init.MemInc = DMA_MINC_ENABLE;
 	hdma1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
 	hdma1.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 	hdma1.Init.Mode = DMA_NORMAL;
 	hdma1.Init.Priority = DMA_PRIORITY_HIGH;
 	hdma1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	hdma1.Init.MemBurst = DMA_MBURST_SINGLE;
 	//hdma1.XferCpltCallback = &DMATransferComplete;
 	HAL_DMA_Init(&hdma1);
 
-	//HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+	HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 }
 
+//Transferring only the first element multiple times, fix this to increment past the initial buffer
 
