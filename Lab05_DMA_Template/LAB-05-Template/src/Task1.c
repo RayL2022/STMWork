b1110f1a03cd5888ea0880331e7e6d595c56b460
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-11-11 00:20:40 -0500

    Update task 1

--- Lab05_DMA_Template/LAB-05-Template/src/Task1.c
+++ Lab05_DMA_Template/LAB-05-Template/src/Task1.c
@@ -3,54 +3,318 @@
 #include<stdlib.h>
 
 DMA_HandleTypeDef hdma1;
-uint8_t data[10] = {0xff, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-uint8_t dma_buffer[10];
-uint8_t c_buffer[10];
+uint32_t cycles;
 
+// For 10 indices buffers
+uint8_t data_10_8[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint16_t data_10_16[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint32_t data_10_32[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+uint8_t dma_buffer_10_8[10];
+uint16_t dma_buffer_10_16[10];
+uint32_t dma_buffer_10_32[10];
+
+uint8_t c_buffer_10_8[10];
+uint16_t c_buffer_10_16[10];
+uint32_t c_buffer_10_32[10];
+
+
+// For 100 indices buffers
+uint8_t data_100_8[100];
+uint16_t data_100_16[100];
+uint32_t data_100_32[100];
+
+uint8_t dma_buffer_100_8[100];
+uint16_t dma_buffer_100_16[100];
+uint32_t dma_buffer_100_32[100];
+
+uint8_t c_buffer_100_8[100];
+uint16_t c_buffer_100_16[100];
+uint32_t c_buffer_100_32[100];
+
+
+// For 1000 indices buffers
+uint8_t data_1000_8[1000];
+uint16_t data_1000_16[1000];
+uint32_t data_1000_32[1000];
+
+uint8_t dma_buffer_1000_8[1000];
+uint16_t dma_buffer_1000_16[1000];
+uint32_t dma_buffer_1000_32[1000];
+
+uint8_t c_buffer_1000_8[1000];
+uint16_t c_buffer_1000_16[1000];
+uint32_t c_buffer_1000_32[1000];
+
+void add_100_lists();
+void add_1000_lists();
+void c_evaluate_time_8(uint8_t* data, uint32_t size);
+void c_evaluate_time_16(uint16_t* data, uint32_t size);
+void c_evaluate_time_32(uint32_t* data, uint32_t size);
+void dma_evaluate_time_8(uint8_t* data, uint32_t size);
+void dma_evaluate_time_16(uint16_t* data, uint32_t size);
+void dma_evaluate_time_32(uint32_t* data, uint32_t size);
 void configureDMA();
+
 
 int main(void)
 {
 	Sys_Init();
 	configureDMA();
 
+	add_100_lists();
+	add_1000_lists();
+
 	// Enable the DWT_CYCCNT register
 	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
 	DWT->LAR = 0xC5ACCE55;
 	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
-	// C-Implementation without DMA
-	DWT->CYCCNT = 0; // Clear the cycle counter
-	for (int i = 0; i < sizeof(data); i++){
-		c_buffer[i] = data[i];
-	}
-	uint32_t cycles = DWT->CYCCNT; // Store the cycle counter
+	c_evaluate_time_8(data_10_8, 10);
+	dma_evaluate_time_8(data_10_8, 10);
+	printf("\r\n");
 
-	DWT->CYCCNT = 0; // Clear the cycle counter
-	printf("Copying data with C-implementation took %lu CPU cycles\r\n",cycles);
-	fflush(stdout);
-	cycles = DWT->CYCCNT; // Store the cycle counter
+	c_evaluate_time_8(data_100_8, 100);
+	dma_evaluate_time_8(data_100_8, 100);
+	printf("\r\n");
 
-	printf("And printing the above took %lu CPU cycles\r\n",cycles);
-	fflush(stdout);
+	c_evaluate_time_8(data_1000_8, 1000);
+	dma_evaluate_time_8(data_1000_8, 1000);
+	printf("\r\n");
 
 
+	c_evaluate_time_16(data_10_16, 10);
+	dma_evaluate_time_16(data_10_16, 10);
+	printf("\r\n");
 
-	DWT->CYCCNT = 0; // Clear the cycle counter
-	HAL_DMA_Start(&hdma1, (uint32_t)&data, (uint32_t)&dma_buffer, 10);
-	HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
-	cycles = DWT->CYCCNT;
+	c_evaluate_time_16(data_100_16, 100);
+	dma_evaluate_time_16(data_100_16, 100);
+	printf("\r\n");
 
-	DWT->CYCCNT = 0; // Clear the cycle counter
-	printf("Copying data with DMA took %lu CPU cycles\r\n",cycles);
-	fflush(stdout);
-	cycles = DWT->CYCCNT; // Store the cycle counter
+	c_evaluate_time_16(data_1000_16, 1000);
+	dma_evaluate_time_16(data_1000_16, 1000);
+	printf("\r\n");
 
-	printf("And printing the above took %lu CPU cycles\r\n",cycles);
-	fflush(stdout);
+
+	c_evaluate_time_32(data_10_32, 10);
+	dma_evaluate_time_32(data_10_32, 10);
+	printf("\r\n");
+
+	c_evaluate_time_32(data_100_32, 100);
+	dma_evaluate_time_32(data_100_32, 100);
+	printf("\r\n");
+
+	c_evaluate_time_32(data_1000_32, 1000);
+	dma_evaluate_time_32(data_1000_32, 1000);
+	printf("\r\n");
+
 
 }
+void add_100_lists(){
+	for (int i = 0; i < 100; i++){
+		data_100_8[i] = i;
+		data_100_16[i] = i;
+		data_100_32[i] = i;
 
+		dma_buffer_100_8[i] = i;
+		dma_buffer_100_16[i] = i;
+		dma_buffer_100_32[i] = i;
+
+		c_buffer_100_8[i] = i;
+		c_buffer_100_16[i] = i;
+		c_buffer_100_32[i] = i;
+	}
+}
+
+void add_1000_lists(){
+	for (int i = 0; i < 1000 ; i++){
+		int j = i;
+		if (j > 255){
+			j = 255;
+		}
+		data_1000_8[i] = j;
+		data_1000_16[i] = j;
+		data_1000_32[i] = j;
+
+		dma_buffer_1000_8[i] = j;
+		dma_buffer_1000_16[i] = j;
+		dma_buffer_1000_32[i] = j;
+
+		c_buffer_1000_8[i] = j;
+		c_buffer_1000_16[i] = j;
+		c_buffer_1000_32[i] = j;
+	}
+}
+
+void c_evaluate_time_8(uint8_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_10_8[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint8_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_100_8[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint8_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_1000_8[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint8_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
+void c_evaluate_time_16(uint16_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_10_16[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint16_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_100_16[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint16_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_1000_16[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint16_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
+
+void c_evaluate_time_32(uint32_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_10_32[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint32_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_100_32[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint32_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		for (int i = 0; i < size; i++){
+			c_buffer_1000_32[i] = data[i];
+		}
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("C-buffer, uint32_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
+
+void dma_evaluate_time_8(uint8_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_10_8, 10);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint8_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_100_8, 100);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint8_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_1000_8, 1000);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint8_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
+
+void dma_evaluate_time_16(uint16_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_10_16, 10);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint16_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_100_16, 100);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint16_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_1000_16, 1000);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint16_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
+
+void dma_evaluate_time_32(uint32_t* data, uint32_t size){
+	if (size == 10){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_10_32, 10);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint32_t, 10 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 100){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_100_32, 100);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint32_t, 100 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+	else if (size == 1000){
+		DWT->CYCCNT = 0; // Clear the cycle counter
+		HAL_DMA_Start(&hdma1, (uint32_t)data, (uint32_t)&dma_buffer_1000_32, 1000);
+		HAL_DMA_PollForTransfer(&hdma1, HAL_DMA_FULL_TRANSFER, HAL_MAX_DELAY);
+		cycles = DWT->CYCCNT; // Store the cycle counter
+		printf("DMA, uint32_t, 1000 elements, %lu CPU cycles\r\n",cycles);
+		fflush(stdout);
+	}
+}
 
 void configureDMA(){
 
@@ -72,6 +336,4 @@ void configureDMA(){
 
 	HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 }
-
-//Transferring only the first element multiple times, fix this to increment past the initial buffer
 
