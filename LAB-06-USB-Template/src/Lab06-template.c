f0756e7d6898cfe2feab23a9e94db7ddfc447d11
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-11-15 19:58:32 -0500

    Update Lab06-template.c

--- LAB-06-USB-Template/src/Lab06-template.c
+++ LAB-06-USB-Template/src/Lab06-template.c
@@ -11,7 +11,9 @@
 #include "ff_gen_drv.h"
 #include "usbh_diskio.h"
 
+HCD_HandleTypeDef husb;
 USBH_HandleTypeDef husbh;
+HID_MOUSE_Info_TypeDef *mouse;
 
 void USBH_UserProcess(USBH_HandleTypeDef *, uint8_t);
 
@@ -21,20 +23,31 @@ int main(void){
 	 // System Initializations
 	Sys_Init();
 
-	// Application Initializations
-	// USBH Driver Initialization
-	// USB Driver Class Registrations: Add device types to handle.
-	// Start USBH Driver
+	// Application Initializations....What are these?
+	#define HAL_HCD_MODULE_ENABLED	// USBH Driver Initialization...Don't think this is right?
+	USBH_RegisterClass(&husbh, USBH_HID_CLASS);	// USB Driver Class Registrations: Add device types to handle.
+	USBH_Start(&husbh); // Start USBH Driver
 
 	while(1){
-		USBH_Process(&husbh);
+		USBH_Process(&husbh);	//What else would be needed if this triggers interrupt?
 		// Other stuff
 	}
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
-	// ...
+	//Why is this needed?
+}
+
+void OTG_HS_IRQ(void){
+	HAL_HCD_IRQHandler(&husb);	//Why this handle when the callbacks use the other handletype?
 }
 
 // Interrupts and Callbacks...
-
+void HAL_PCD_ConnectCallback(PCD_HandleTypeDef *hpcd){
+	mouse = USBH_HID_GetMouseInfo(&husbh);
+	int xval = mouse->x;
+	int yval = mouse->y;
+	printf("X value moved: %d, Y value moved: %d, Button 1: %d, Button 2: %d, Button 3: %d", xval, yval, \
+			mouse->buttons[0], mouse->buttons[1], mouse->buttons[2]);
+}
+void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef *hpcd);
