3fc0ea259a9f9893c7aa171336c26f08830f7a6b
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-11-27 17:40:48 -0500

    Update Lab06-template.c

--- LAB-06-USB-Template/src/Lab06-template.c
+++ LAB-06-USB-Template/src/Lab06-template.c
@@ -13,39 +13,31 @@
 
 USBH_HandleTypeDef husbh;
 HID_MOUSE_Info_TypeDef *mouse;
-int connected = 0;
 
 void USBH_UserProcess(USBH_HandleTypeDef *, uint8_t);
-
-
 
 int main(void){
 	 // System Initializations
 	Sys_Init();
-	//HAL_NVIC_EnableIRQ(OTG_HS_IRQn);
-	// Application Initializations
-	//#define HAL_HCD_MODULE_ENABLED	// USBH Driver Initialization...Don't think this is right?
-	USBH_Init(&husbh, USBH_UserProcess, 0);
-	USBH_RegisterClass(&husbh, USBH_HID_CLASS);	// USB Driver Class Registrations: Add device types to handle.
+	USBH_Init(&husbh, USBH_UserProcess, 0);	//Initialize HID USB Handle
+	USBH_RegisterClass(&husbh, USBH_HID_CLASS);	// USB Driver Class Registrations: Add HID to handle.
 	USBH_Start(&husbh); // Start USBH Driver
 	fflush(stdout);
-	printf("start\n\r");
 	while(1){
-		//printf("while\n\r");
 		USBH_Process(&husbh);
 	}
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
-
+	//Connected to USBH_Process(&husbh) function call
 }
 
 void USBH_HID_EventCallback(USBH_HandleTypeDef *phost){
-	if (USBH_HID_GetDeviceType(phost) == HID_MOUSE){
-		mouse = USBH_HID_GetMouseInfo(&husbh);
+	if (USBH_HID_GetDeviceType(phost) == HID_MOUSE){	//If a mouse is connected
+		mouse = USBH_HID_GetMouseInfo(&husbh);	//Get mouse info
 		int xval = mouse->x;
-		int yval = mouse->y;
+		int yval = mouse->y;	//Assign info from mouse to variables
 		printf("X value moved: %d, Y value moved: %d, Button 1: %d, Button 2: %d, Button 3: %d\n\r", xval, yval, \
-				mouse->buttons[0], mouse->buttons[1], mouse->buttons[2]);
+				mouse->buttons[0], mouse->buttons[1], mouse->buttons[2]);	//Print Mouse Info
 	}
 }
