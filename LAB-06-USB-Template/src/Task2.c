6b0a681110905f7d0b3afa190e2973401d59a61b
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-11-29 18:30:26 -0500

    t2

--- LAB-06-USB-Template/src/Task2.c
+++ LAB-06-USB-Template/src/Task2.c
@@ -44,6 +44,7 @@ int main(void){
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
 	if (phost->pActiveClass == USBH_MSC_CLASS){
+		//Elm-Chan.org Code Example
 	    res = f_mount(fs, "0:/", 1);                    /* Mount the default drive */
 	    if (res == FR_OK) {
 	    	//printf("Entered\r\n");
@@ -65,21 +66,22 @@ void USBH_HID_EventCallback(USBH_HandleTypeDef *phost){
 	}
 }
 
+//Elm-Chan.org Code Example
 FRESULT scan_files (char* path /* Start node to be scanned (***also used as work area***) */){
     res = f_opendir(&dir, path);                       /* Open the directory */
     if (res == FR_OK) {
-    	printf("Entered\r\n");
         for (;;) {
             res = f_readdir(&dir, &fno);                   /* Read a directory item */
             if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
             if (fno.fattrib & AM_DIR) {                    /* It is a directory */
                 i = strlen(path);
                 sprintf(&path[i], "/%s", fno.fname);
-                res = scan_files(path);                    /* Enter the directory */
+                printf("Directory: %s/%s\r\n", path, fno.fname);
+                //res = scan_files(path);                    /* Enter the directory */
                 if (res != FR_OK) break;
                 path[i] = 0;
             } else {                                       /* It is a file. */
-                printf("%s/%s\n", path, fno.fname);
+                printf("File: %s/%s\r\n", path, fno.fname);
             }
         }
         f_closedir(&dir);
@@ -87,5 +89,4 @@ FRESULT scan_files (char* path /* Start node to be scanned (***also used as work
     return res;
 }
 
-//FIX LINE 74
 
