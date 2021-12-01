11aaeac8a72a8fe9476ff1c5d67e5fc18b3dfb8b
RayL2022 <91843577+RayL2022@users.noreply.github.com>
2021-12-01 17:30:08 -0500

    t2

--- LAB-06-USB-Template/src/Task2.c
+++ LAB-06-USB-Template/src/Task2.c
@@ -2,7 +2,7 @@
  * Task2.c
  *
  *  Created on: Nov 22, 2021
- *      Author: Shayne
+ *      Author: Ray & Shayne
  */
 #include "init.h"
 #include "usbh_conf.h"
@@ -16,80 +16,62 @@ USBH_HandleTypeDef husbh;
 HID_MOUSE_Info_TypeDef *mouse;
 int connected = 0;
 
-FATFS *fs;
-FRESULT res;
-DIR dir;
-UINT i;
-static FILINFO fno;
-char buff[256];
+FATFS *fs; //File System Structure/Workspace
+FRESULT fr; //Check return status of function runs
+DIR dir; //Directory Object Structure
+FILINFO fno; //File Information Structure
 
 void USBH_UserProcess(USBH_HandleTypeDef *, uint8_t);
-FRESULT scan_files (char* path);
 
 int main(void){
-	 // System Initializations
+	// System Initializations
 	Sys_Init();
-	USBH_Init(&husbh, USBH_UserProcess, 0);
-	USBH_RegisterClass(&husbh, USBH_HID_CLASS);	// USB Driver Class Registrations: Add device types to handle.
-	USBH_RegisterClass(&husbh, USBH_MSC_CLASS);	// #1
-	FATFS_LinkDriver(&USBH_Driver, "0:/"); // #2
-	USBH_Start(&husbh); // Start USBH Driver
+
+	USBH_Init(&husbh, USBH_UserProcess, 0); //Initialize HID USB Handle
+
+	USBH_RegisterClass(&husbh, USBH_HID_CLASS);	//Ability to handle Human Interface Devices (HID)
+	USBH_RegisterClass(&husbh, USBH_MSC_CLASS);	//Ability to handle Mass Storage Class (MSC) device types
+
+	FATFS_LinkDriver(&USBH_Driver, "0:/"); //Link our connected first device ("0:/") to the FATFS driver
+	USBH_Start(&husbh); //Start USBH Driver
 	fflush(stdout);
-	fs = malloc(sizeof (FATFS));           /* Get work area for the volume */
+	fs = malloc(sizeof (FATFS)); //Get work area for the volume
 
     while(1){
-		USBH_Process(&husbh);
+		USBH_Process(&husbh); //Poll for USB updates
 	}
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
+	//Check to see the flash drive or MSC device is connected
 	if (phost->pActiveClass == USBH_MSC_CLASS){
-		//Elm-Chan.org Code Example
-	    res = f_mount(fs, "0:/", 1);                    /* Mount the default drive */
-	    if (res == FR_OK) {
-	    	//printf("Entered\r\n");
-	        strcpy(buff, "/");
-	        res = scan_files(buff);
+		//Modified From Elm-Chan.org f_findfirst Code Example
+	    fr = f_mount(fs, "0:/", 0); //Mount the default drive
+	    if (fr == FR_OK) { //No errors with mounting
+	        fr = f_findfirst(&dir, &fno, "0:/", "*"); //Start to search for any file
+	        while (fr == FR_OK && fno.fname[0]){ //Break when error or no more items matched
+	            if (fno.fattrib & AM_DIR) { //It is a directory
+		            printf("Directory: %s\r\n", fno.fname); //Print the object name
+	            }
+	            else{ //It is a file
+		            printf("File: %s\r\n", fno.fname); //Print the object name
+	            }
+	            fr = f_findnext(&dir, &fno); //Search for next item
+	        }
+	        f_closedir(&dir); //Close directory
 	    }
 	}
 }
 
 void USBH_HID_EventCallback(USBH_HandleTypeDef *phost){
-	if (phost->pActiveClass == USBH_HID_CLASS){
-		if (USBH_HID_GetDeviceType(phost) == HID_MOUSE){ //#3
-			mouse = USBH_HID_GetMouseInfo(&husbh);
+	if (phost->pActiveClass == USBH_HID_CLASS){ //HID connected
+		if (USBH_HID_GetDeviceType(phost) == HID_MOUSE){ //If a mouse is Connected
+			mouse = USBH_HID_GetMouseInfo(&husbh); //Get mouse info
 			int xval = mouse->x;
-			int yval = mouse->y;
+			int yval = mouse->y; //Assign info from mouse to variables
 			printf("X value moved: %d, Y value moved: %d, Button 1: %d, Button 2: %d, Button 3: %d\n\r", xval, yval, \
-					mouse->buttons[0], mouse->buttons[1], mouse->buttons[2]);
+					mouse->buttons[0], mouse->buttons[1], mouse->buttons[2]); //Print Mouse Info
 		}
 	}
 }
-
-//Elm-Chan.org Code Example
-FRESULT scan_files (char* path /* Start node to be scanned (***also used as work area***) */){
-    res = f_opendir(&dir, path);                       /* Open the directory */
-    if (res == FR_OK) {
-        for (;;) {
-            res = f_readdir(&dir, &fno);                   /* Read a directory item */
-            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
-            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
-                i = strlen(path);
-                sprintf(&path[i], "/%s", fno.fname);
-                printf("Directory: %s/%s\r\n", path, fno.fname);
-
-/*              //res = scan_files(path);                    //Enter the directory
-                if (res != FR_OK) break;
-                path[i] = 0;*/
-            }
-            else {                                        //It is a file.
-                printf("File: %s/%s\r\n", path, fno.fname);
-            }
-
-        }
-        f_closedir(&dir);
-    }
-    return res;
-}
-
 
