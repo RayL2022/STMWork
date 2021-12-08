4eb4be56461f2bd066c7c6bebb85c3e505ad0136
shayneEERPI <90424467+shayneEERPI@users.noreply.github.com>
2021-12-08 14:45:30 -0500

    Timer

---
+++ FinalTemplate/inc/Timer.h
@@ -0,0 +1,28 @@
+#ifndef TIMER_H_
+#define	TIMER_H_
+
+#include "stm32f769xx.h"
+#include "stm32f7xx_hal.h"
+#include "uart.h"
+#include "init.h"
+
+TIM_HandleTypeDef htim7;
+volatile uint32_t elapsed;
+volatile uint32_t one;
+volatile uint32_t saved;
+
+char minute;
+char second;
+char input2;
+char start;
+char counts;
+char score;
+char speedx;
+char speedy;
+
+//Function Prototypes
+void Init_Timer();
+void select_time();
+void speed();
+
+#endif
--- FinalTemplate/src/Timer.c
+++ FinalTemplate/src/Timer.c
@@ -4,31 +4,15 @@
  *  Created on: Nov 30, 2021
  *      Author: Shayne
  */
-#include "init.h"
-#include "stm32f7xx_hal_conf.h"
-void Init_Timer();
-
-TIM_HandleTypeDef htim7;
-volatile uint32_t elapsed = 0;
-
-int main() {
-	Sys_Init();
-	HAL_Init();
-	Init_Timer();
-
-	fflush(stdout);
-	while (1){
-		printf("Elapsed: %ld\n\r", elapsed);
-	}
-
-}
+#include "Timer.h"
+#include "Draw.h"
 
 void Init_Timer() {
 	__HAL_RCC_TIM7_CLK_ENABLE(); //Enable the TIM7 peripheral
 
 	htim7.Instance = TIM7;
-	htim7.Init.Prescaler = 499;
-	htim7.Init.Period = 21599; //.1sec
+	htim7.Init.Prescaler = 999;
+	htim7.Init.Period = 10799; //0.1sec
 
 	HAL_NVIC_EnableIRQ(TIM7_IRQn);
 
@@ -37,10 +21,68 @@ void Init_Timer() {
 }
 
 
+// -- User Functions -------------
+//
+void select_time() {
+	printf("\033[12;40HEnter: \n\r");
+	while (input2 == 0){
+		input2 = getchar();
+		printf("%d\n\r", input2);
+	}
+	printf("Input: %c\n\r", input2);
+	if (input2 == '5'){
+		printf("\033[2J"); fflush(stdout);
+		elapsed = 300;
+		start = 1;
+		counts = 1;
+	}
+
+	if (input2 == '3'){
+		printf("\033[2J"); fflush(stdout);
+		elapsed = 180;
+		start = 1;
+		counts = 1;
+	}
+
+	if (input2 == '1'){
+		printf("\033[2J"); fflush(stdout);
+		elapsed = 60;
+		start = 1;
+		counts = 1;
+	}
+}
+
+void count_down(){
+	minute = elapsed/60;
+	second = (elapsed%60);
+	update_score_and_time(p1, p2, minute, second);
+}
+
+void speed(){
+	//Needs edits the values are not correct. Thats how many pixel/s/10 but each pixel movement must be seen
+	if ((one-saved) > 20){
+		saved = one;
+		if (play_ball.x_speed < 0){
+			if(play_ball.x_speed > MIN_X_SPEED){
+				play_ball.x_speed --;
+			}
+			int speedUP = -1000/play_ball.x.speed;
+			HAL_Delay(speedUP);
+			play_ball = update_ball(play_ball);
+		}
+		else if (play_ball.x_speed > 0){
+			if(play_ball.x_speed < MAX_X_SPEED){
+				play_ball.x_speed ++;
+			}
+			HAL_Delay(1000/play_ball.x_speed);
+			play_ball = update_ball(play_ball);
+		}
+	}
+
+}
 // -- ISRs (IRQs) -------------
 //
 void TIM7_IRQHandler() {
-	elapsed++;  //increment the variable for time elapsed
 	HAL_TIM_IRQHandler(&htim7);
 }
 
@@ -49,11 +91,31 @@ void TIM7_IRQHandler() {
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 // This callback is automatically called by the HAL on the UEV event
 	if(htim->Instance == TIM7){
-		elapsed++;  //increment the variable for time elapsed
+		if ((start == 1)){
+			if (one == 0){
+				score1 = p1.score;
+				score2 = p2.score;
+				printf("\033[13;38H\033[;40m "); fflush(stdout);
+				printf("%ld:00", elapsed/60); fflush(stdout);
+			}
+			one++;
+			if (p1.score != score1){
+				saved = one;
+				score1=p1.score;
+			}
+			if (p2.score != score2){
+				saved = one;
+				score2=p2.score;
+			}
+			speed();
+			if ((one%10 == 0)){
+				elapsed--;  //Decrement elapsed
+				count_down();
+			}
+		}
 	}
-
-	}
-
+}
 
 void HAL_TIMEx_BreakCallback(TIM_HandleTypeDef *htim){}
 void HAL_TIMEx_CommutationCallback(TIM_HandleTypeDef *htim){}
+
